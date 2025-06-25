0dbea0e0aad008e91d5e0ece2b0d5a2d005429f0
Md Towfik Omer <mdtowfikomer@gmail.com>
2025-06-26 00:56:27 +0530

    Implementation of Tree

---
+++ Trees/implementationTree.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(vector<int> nums){
+    idx++;
+    if(nums[idx] == -1){
+        return NULL;
+    }
+
+    Node* curNode = new Node(nums[idx]);
+    curNode->left = buildTree(nums);
+    curNode->right = buildTree(nums);
+
+    return curNode;
+
+}
+
+
+int main(){
+    vector<int> nums = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node* root = buildTree(nums);
+    cout << "Root node data is " << root->data << endl;
+
+    return 0;
+}
