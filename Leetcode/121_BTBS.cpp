d46d9fb52692645601e29884f4c855a82af04d8c
Md Towfik Omer <mdtowfikomer@gmail.com>
2025-12-30 19:19:08 +0530

    added Leetcode-121

---
+++ Leetcode/121_BTBS.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+    int maxProfit = 0;
+    if (prices.empty()) {
+        return 0;
+    }
+    int bestBuy = prices[0];
+
+    for(size_t i = 0; i < prices.size(); i++){
+        if(prices[i] > bestBuy){
+        maxProfit = max(maxProfit, prices[i]-bestBuy);
+        }
+        bestBuy = min(bestBuy, prices[i]);
+    }   
+    return maxProfit;
+}
+
+int main() {
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "Max profit: " << maxProfit(prices) << endl;
+    return 0;
+}
